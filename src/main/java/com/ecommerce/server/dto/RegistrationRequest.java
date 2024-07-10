24a3b73fc252f7c80e15c7cd79a59ae8c2e9cc8f
eltaeif-houssem <houssem.developer@gmail.com>
2024-07-10 05:27:34 +0100

    adding authentication service and the necessary dto's

---
+++ src/main/java/com/ecommerce/server/dto/RegistrationRequest.java
@@ -0,0 +1,37 @@
+package com.ecommerce.server.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Builder
+public class RegistrationRequest {
+    @NotEmpty(message = "Firstname is required")
+    @NotNull(message = "Firstname is required")
+    private String firstname;
+
+    @NotEmpty(message = "Lastname is required")
+    @NotNull(message = "Lastname is required")
+    private String lastname;
+
+    @Email(message = "Email is not well formatted")
+    @NotEmpty(message = "Email is required")
+    @NotNull(message = "Email is required")
+    private String email;
+
+    @NotEmpty(message = "Phone is required")
+    @NotNull(message = "Phone is required")
+    private String phone;
+
+    @NotEmpty(message = "Password is required")
+    @NotNull(message = "Password is required")
+    @Size(min = 8, message = "Password should be 8 characters long minimum")
+    private String password;
+}
---
+++ src/main/java/com/ecommerce/server/service/AuthenticationService.java
@@ -0,0 +1,66 @@
+package com.ecommerce.server.service;
+
+import com.ecommerce.server.dto.AuthenticationRequest;
+import com.ecommerce.server.dto.AuthenticationResponse;
+import com.ecommerce.server.dto.RegistrationRequest;
+import com.ecommerce.server.enums.RoleName;
+import com.ecommerce.server.model.User;
+import com.ecommerce.server.repository.RoleRepository;
+import com.ecommerce.server.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    private final RoleRepository roleRepository;
+
+    @Value("${application.mailing.frontend.activation-url}")
+    private String activationUrl;
+
+    public void register(RegistrationRequest request) {
+        var userRole = roleRepository.findByName(RoleName.ROLE_CLIENT)
+                // todo - better exception handling
+                .orElseThrow(() -> new IllegalStateException("ROLE CLIENT was not initiated"));
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .phone(request.getPhone())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .roles(List.of(userRole))
+                .build();
+        userRepository.save(user);
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        var auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        var claims = new HashMap<String, Object>();
+        var user = ((User) auth.getPrincipal());
+        claims.put("fullName", user.getFullName());
+
+        var jwtToken = jwtService.generateToken(claims, (User) auth.getPrincipal());
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
