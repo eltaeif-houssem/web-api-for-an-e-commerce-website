8e531e9b85e493d9351f8d3e2c262024f81c574e
eltaeif-houssem <houssem.developer@gmail.com>
2024-07-10 05:30:42 +0100

    adding the authentication rest controller

---
+++ src/main/java/com/ecommerce/server/controller/AuthenticationController.java
@@ -0,0 +1,42 @@
+package com.ecommerce.server.controller;
+
+
+import com.ecommerce.server.dto.AuthenticationRequest;
+import com.ecommerce.server.dto.AuthenticationResponse;
+import com.ecommerce.server.dto.RegistrationRequest;
+import com.ecommerce.server.service.AuthenticationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseEntity<?> register(
+            @RequestBody @Valid RegistrationRequest request
+    ) {
+
+        return ResponseEntity.accepted().build();
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+
+}
