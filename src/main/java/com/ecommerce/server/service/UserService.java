69c2681701256e138c3fe4dc59c823a4a770b8f2
eltaeif-houssem <houssem.developer@gmail.com>
2024-07-16 04:19:10 +0100

    add get current connected user fn

--- src/main/java/com/ecommerce/server/service/UserService.java
+++ src/main/java/com/ecommerce/server/service/UserService.java
@@ -1,5 +1,7 @@
 package com.ecommerce.server.service;
 
+import com.ecommerce.server.exception.NotFoundException;
+import com.ecommerce.server.model.User;
 import com.ecommerce.server.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -7,13 +9,23 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
 
-    public Principal getCurrentUser() {
+    public User getCurrentUser() {
+        String userEmail = getUserPrincipal().getName();
+        Optional<User> user = userRepository.findByEmail(userEmail);
+        if(user.isEmpty()){
+            throw new NotFoundException("User not found!");
+        }
+        return user.get();
+    }
+
+    public Principal getUserPrincipal() {
         return SecurityContextHolder.getContext().getAuthentication();
     }
 }
